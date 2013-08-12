06f902e2fb88fc7a02bfbca8303e8904f9662f72
Regis FLORET <regis.floret@gmail.com>
2013-08-12 10:11:58 +0400

    Change where the plugin menu is displayed. Now in 'Blog' section

--- _admin.php
+++ _admin.php
@@ -20,7 +20,7 @@
  *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA    *
 \***************************************************************/
 
-$_menu['Plugins']->addItem(
+$_menu['Blog']->addItem(
     __('Webmaster Tools'),
     'plugin.php?p=WebmasterTools',
     'index.php?pf=WebmasterTools/icon.png',
